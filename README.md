fedc7c9e1dc121d96f72ab974f48fd6f389ce012
tkrsivasakthi <tkrsivasakthi@gmail.com>
2023-06-03 00:03:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-Change-detection
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-component-dkavpx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-router-sample';
+  get test() {
+    console.log('test AppComponent')
+    return 1;
+  }
+  ngOnInit() {
+    console.log('app')
+  }
+  clickA() {}
+}
+ 
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
